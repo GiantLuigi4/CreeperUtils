811b7e76f8a7ccc93a26a1c814a5154ab8b53478
GiantLuigi4 <tjmferet@hotmail.com>
2020-09-11 16:32:29 -0400

    added a better way to construct a dependency manager that starts with dependencies

--- src/main/java/com/tfc/utils/_flame/dependency_management/Manager.java
+++ src/main/java/com/tfc/utils/_flame/dependency_management/Manager.java
@@ -6,10 +6,12 @@ import com.tfc.flame.FlameURLLoader;
 import com.tfc.utils._flame.FlameLoader;
 
 import java.io.File;
+import java.lang.reflect.InvocationTargetException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
 
 public class Manager {
 	private final FlameLoader loader;
@@ -20,6 +22,82 @@ public class Manager {
 	
 	public Manager(String jarName) throws MalformedURLException {
 		this.loader = new FlameLoader(jarName);
+	}
+	
+	public static Manager constructFromDependencies(Class<FlameLoader> loader, String URLDeps, String gradleLikeDeps, File[] otherDeps) throws MalformedURLException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+		ArrayList<URL> urls = new ArrayList<>();
+		String repo = "";
+		for (String s : gradleLikeDeps.split(",")) {
+			try {
+				if (repo.equals("")) {
+					repo = s;
+				} else {
+					System.out.println(repo);
+					System.out.println(s);
+					String s1 = s.replace("", "" + File.separatorChar);
+					String[] info = s1.split(":", 3);
+					info[2] = info[2].replace("" + File.separatorChar, "");
+					s1 = info[0] + File.separatorChar + info[2] + File.separatorChar + info[1] + File.separatorChar + info[1] + "-" + info[2] + ".jar";
+					String urlS = info[0] + File.separatorChar + info[1] + File.separatorChar + info[2] + File.separatorChar + info[1] + "-" + info[2] + ".jar";
+					File output = new File((com.tfc.utils.Files.dir + File.separatorChar + "libs" + File.separatorChar + s1).replace(".jar", ".zip"));
+					if (!output.exists()) {
+						output.getParentFile().mkdirs();
+						output.createNewFile();
+						
+						String url = repo + (urlS);
+						try {
+							URL url1 = new URL(url.replace("" + File.separatorChar, "/"));
+							System.out.println(url1.toString());
+							//https://stackabuse.com/how-to-download-a-file-from-a-url-in-java/
+							Files.copy(url1.openStream(), output.toPath(), StandardCopyOption.REPLACE_EXISTING);
+						} catch (Throwable err) {
+							FlameConfig.logError(err);
+						}
+					}
+					urls.add(output.toURL());
+					repo = "";
+				}
+			} catch (Throwable err) {
+				FlameConfig.logError(err);
+				repo = "";
+			}
+		}
+		
+		String file = "";
+		for (String s : URLDeps.toString().split(",")) {
+			try {
+				if (file.equals("")) {
+					file = s;
+				} else {
+					File output = new File((com.tfc.utils.Files.dir + File.separatorChar + file.replace("/", "" + File.separatorChar).replace(".jar", ".zip")));
+					if (!output.exists()) {
+						output.getParentFile().mkdirs();
+						output.createNewFile();
+						
+						String url = s;
+						try {
+							URL url1 = new URL(url.replace("" + File.separatorChar, "/"));
+							System.out.println(url1.toString());
+							//https://stackabuse.com/how-to-download-a-file-from-a-url-in-java/
+							Files.copy(url1.openStream(), output.toPath(), StandardCopyOption.REPLACE_EXISTING);
+						} catch (Throwable err) {
+							FlameConfig.logError(err);
+						}
+					}
+					urls.add(output.toURL());
+					file = "";
+				}
+			} catch (Throwable err) {
+				FlameConfig.logError(err);
+				file = "";
+			}
+		}
+		
+		for (File f : otherDeps) urls.add(f.toURL());
+		
+		URL[] urls1 = new URL[urls.size()];
+		for (int i = 0; i < urls.size(); i++) urls1[i] = urls.get(i);
+		return new Manager(loader.getConstructor(URL[].class).newInstance((Object) urls1));
 	}
 	
 	public Manager(FlameURLLoader loader) {
@@ -67,7 +145,7 @@ public class Manager {
 	
 	public void addFromURL(String dependencies) {
 		String file = "";
-		for (String s : dependencies.toString().split(",")) {
+		for (String s : dependencies.split(",")) {
 			try {
 				if (file.equals("")) {
 					file = s;
